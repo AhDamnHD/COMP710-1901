7bc4859e42db9bb395bdb2cce920dc1cf91ef037
Adam Halley <Ag3ntMayn3@gmail.com>
2019-02-21 19:57:54 +0000

    start of africa

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+
+<div class="wrapper">
+    <div class="title">
+    <?php
+        echo "<h1>Africa By Toto</h1>";
+        echo "<hr/>";
+        echo "<p>I hear the drums echoing tonight</p>";
+        
+    ?>
+    </div>
+<a href="profile.html">Visit my profile.</a>
+</div>
+
+</body>
+</html>
+
+
+
